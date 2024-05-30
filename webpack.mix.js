32b93d490de1df858cb2f520596ea457451a26c2
hekuranzabergja <hekurannzabergja@gmail.com>
2024-05-30 16:33:15 +0200

    fixed scss

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,3 +15,5 @@ mix.js('resources/js/app.js', 'public/assets/js/argon-dashboard.js')
     .sass('resources/scss/argon-dashboard.scss', 'public/assets/css/argon-dashboard.css', [
         //
     ]);
+
+    
