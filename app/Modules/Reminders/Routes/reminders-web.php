4f06eb4ed715371d0d97a6dd7c429a060caa30fe
dmaloku <drilon@minabastapolare.se>
2025-04-27 15:47:13 +0200

    fixed routes

--- app/Modules/Reminders/Routes/reminders-web.php
+++ app/Modules/Reminders/Routes/reminders-web.php
@@ -3,13 +3,12 @@
 use App\Modules\Reminders\Controllers\RemindersController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['web', 'auth'])->group(function () {
-    Route::prefix('reservations/{reservationId}/reminders')->group(function () {
-        Route::get('/', [RemindersController::class, 'index'])->name('reminders.index');
-        Route::get('/create', [RemindersController::class, 'create'])->name('reminders.create');
-        Route::post('/', [RemindersController::class, 'store'])->name('reminders.store');
-        Route::get('/{id}/edit', [RemindersController::class, 'edit'])->name('reminders.edit');
-        Route::put('/{id}', [RemindersController::class, 'update'])->name('reminders.update');
-        Route::delete('/{id}', [RemindersController::class, 'destroy'])->name('reminders.destroy');
-    });
-}); 
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/reservations/{reservation}/reminders', [RemindersController::class, 'index'])->name('reminders.index');
+    Route::get('/reservations/{reservation}/reminders/create', [RemindersController::class, 'create'])->name('reminders.create');
+    Route::post('/reservations/{reservation}/reminders', [RemindersController::class, 'store'])->name('reminders.store');
+    Route::get('/reservations/{reservation}/reminders/{reminder}/show', [RemindersController::class, 'show'])->name('reminders.show');
+    Route::get('/reservations/{reservation}/reminders/{reminder}/edit', [RemindersController::class, 'edit'])->name('reminders.edit');
+    Route::put('/reservations/{reservation}/reminders/{reminder}', [RemindersController::class, 'update'])->name('reminders.update');
+    Route::delete('/reservations/{reservation}/reminders/{reminder}', [RemindersController::class, 'destroy'])->name('reminders.destroy');
+});
--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,6 @@ use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Modules\Notifications\Controllers\NotificationsController;
-use App\Modules\Events\Controllers\EventsController;
-use App\Modules\Reminders\Controllers\RemindersController;
 
 /*
 |--------------------------------------------------------------------------
@@ -103,13 +101,3 @@ Route::get('files/{path}', [\App\Modules\Files\Controllers\AppFileController::cl
 Route::put('/reservations/manage-guests/{reservationId}/update/{guestId}', 'ReservationsController@updateGuest')
     ->name('reservations.updateGuest');
 
-// Reminders
-Route::group(['middleware' => 'auth'], function () {
-    Route::get('/reservations/{reservation}/reminders', [RemindersController::class, 'index'])->name('reminders.index');
-    Route::get('/reservations/{reservation}/reminders/create', [RemindersController::class, 'create'])->name('reminders.create');
-    Route::post('/reservations/{reservation}/reminders', [RemindersController::class, 'store'])->name('reminders.store');
-    Route::get('/reservations/{reservation}/reminders/{reminder}/show', [RemindersController::class, 'show'])->name('reminders.show');
-    Route::get('/reservations/{reservation}/reminders/{reminder}/edit', [RemindersController::class, 'edit'])->name('reminders.edit');
-    Route::put('/reservations/{reservation}/reminders/{reminder}', [RemindersController::class, 'update'])->name('reminders.update');
-    Route::delete('/reservations/{reservation}/reminders/{reminder}', [RemindersController::class, 'destroy'])->name('reminders.destroy');
-});
